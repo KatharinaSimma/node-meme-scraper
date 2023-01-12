97c85bacddf67555858d00ffa79a072bdf98ca6a
Katharina Simma <katharina.simma@diebin.at>
2023-01-12 15:48:05 +0100

    Add fancy progress bar

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 import fs from 'node:fs';
 import { get } from 'node:https';
+import colors from 'ansi-colors';
 import bl from 'bl';
+import cliProgress from 'cli-progress';
 import { parse } from 'node-html-parser';
 
 const website = 'https://memegen-link-examples-upleveled.netlify.app/';
 const numberOfPics = 10;
 
+// const bar1 = new cliProgress.SingleBar({}, cliProgress.Presets.rect);
+const bar1 = new cliProgress.SingleBar({
+  format:
+    'Memes downloaded ' +
+    colors.bold.magentaBright('{bar}') +
+    ' {percentage}% | {value}/{total} files',
+  barCompleteChar: '#',
+  barIncompleteChar: '-',
+  hideCursor: true,
+});
+bar1.start(numberOfPics, 0);
+
+// set up the memes folder
 if (fs.existsSync('./memes')) {
   // Do nothing. Any files in the folder will be overwritten.
 } else {
   fs.mkdirSync('./memes');
 }
 
+// fetch the memes
 get(website, (response) => {
   response.pipe(
     bl(function (err, data) {
@@ -30,7 +46,12 @@ get(website, (response) => {
               fs.writeFile(
                 `./memes/${(i + 1).toString().padStart(2, 0)}.jpg`,
                 imageData,
-                () => {},
+                () => {
+                  bar1.update(i + 1);
+                  if (i + 1 === numberOfPics) {
+                    bar1.stop();
+                  }
+                },
               );
             }),
           );
